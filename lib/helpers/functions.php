d18e2bee8f1a5fca24243a0509488d4f40cecfa0
pxgamer <owzie123@gmail.com>
2018-01-04 10:42:03 +0000

    Added autoloading of helpers file

---
+++ lib/helpers/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!function_exists('strToHex')) {
+    function strToHex($string)
+    {
+        $hex = '';
+        for ($i = 0; $i < strlen($string); $i++) {
+            $hex .= dechex(ord($string[$i]));
+        }
+        return $hex;
+    }
+}
